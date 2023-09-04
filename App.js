1283774978d927a86dc8d0942972bf29431aaff9
Ashraful Hoda <141621146+ashrafulHodaJs@users.noreply.github.com>
2023-09-04 08:59:18 +0530

    Create App.js

---
+++ App.js
@@ -0,0 +1,26 @@
+import "./styles.css";
+import { useEffect, useState } from "react";
+/* Styles for the h1 element */
+export default function App() {
+  const [advice, setAdvice] = useState("");
+  const [count, setCount] = useState(0);
+
+  async function getAdvice() {
+    const res = await fetch("https://api.adviceslip.com/advice");
+    const data = await res.json();
+    setAdvice(data.slip.advice);
+    setCount((c) => c + 1);
+  }
+
+  useEffect(function () {
+    getAdvice();
+  }, []);
+
+  return (
+    <div>
+      <h1>{advice}</h1>
+      <button onClick={getAdvice}>Get advice</button>
+      <Message count={count} />
+    </div>
+  );
+}
